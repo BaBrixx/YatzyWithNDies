e7f846e343bf66fa040b367c859730b63ab58d8c
Bastian Brix Hansen <bastian.b.hansen@hotmail.com>
2021-10-29 13:44:10 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+//  main.c
+//  Yatzy with N dies
+//
+//  Created by Bastian Brix Hansen on 29.10.2021.
+//  Copyright © 2021 Bastian Brix Hansen. All rights reserved.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    
+    
+    return EXIT_SUCCESS;
+}
